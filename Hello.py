8d43875814e2ffc12d36089df7e97c6fe9272f09
Jacoblee <jacobleeloveru@gmail.com>
2017-06-15 11:13:46 +0800

    Initial commit

---
+++ Hello.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# print ('Please enter your Weight: ')
+# weight = (float)( input())
+# print ('Please enter your Height: ')
+# height = (float)( input())
+#
+# ratio = (weight / height) * (weight / height)
+#
+# if ratio < 18.5:
+#     print ('Too light!')
+# elif 25 > ratio >= 18.5:
+#     print ('formal')
+# elif 28 > ratio >= 25:
+#     print ('More Heavy')
+# elif 32 > ratio >= 28:
+#     print ('Fatter')
+# else:
+#     print ('Heavily fat!')
+print ("hello world!")
