f0c1e159b492695881f65b700011b1a0ac0c111d
Jacoblee <jacobleeloveru@gmail.com>
2017-06-15 21:12:37 +0800

    some demos

--- Hello.py
+++ Hello.py
@@ -16,4 +16,23 @@
 #     print ('Fatter')
 # else:
 #     print ('Heavily fat!')
-print ("hello world!")
+# print ("hello world!")
+# print 2.5 + 10.0/4
+# a = 'python'
+# print 'hello,', a or 'world'
+# b = ''
+# print 'hello,', b or 'world'
+# L = ['Adam', 95.5, 'Lisa', 85, 'Bart', 59]
+# L.insert(2,'Paul')
+# L = ['Adam', 'Lisa', 'Paul', 'Bart']
+# L.pop(2)
+# L.pop(2);
+# print L;
+# L = ['Adam', 'Lisa', 'Bart']
+# L[-1] = 'Adam'
+# L[0] = 'Bart'
+# print L
+t= (0,1,2,3,4,5,6,7,8,9)
+print t
+t = ('Adam',)
+print t
